9dd9bb9f0ecd428684ec6642097a99d220d7bdc9
Eli Lima <elimigueldelima@gmail.com>
2024-08-27 16:50:46 +0100

    chore: remove old unused keycloak libs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,34 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthGuard, KeycloakConnectModule, PolicyEnforcementMode, ResourceGuard, RoleGuard, TokenValidation } from 'nest-keycloak-connect';
-import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [KeycloakConnectModule.register({
-    authServerUrl: 'http://localhost:8080', // might be http://localhost:8080/auth for older keycloak versions
-    realm: 'cloud-backup-system',
-    clientId: 'api-service',
-    secret: "eRtr2j2nFVIPj2gCtymPqp9NjIyCpPsS",
-    policyEnforcement: PolicyEnforcementMode.PERMISSIVE, // optional
-    tokenValidation: TokenValidation.ONLINE, // optional
-  }), AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController],
-  providers: [
-    {
-      provide: APP_GUARD,
-      useClass: AuthGuard,
-    },
-    {
-      provide: APP_GUARD,
-      useClass: ResourceGuard,
-    },
-    {
-      provide: APP_GUARD,
-      useClass: RoleGuard,
-    },
-    AppService],
+  providers: [AppService],
 })
 export class AppModule {}
